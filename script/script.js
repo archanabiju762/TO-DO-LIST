340f8174d568e7e1f8a5b7f088fabe552fd98578
Archana Biju <archanabiju3110@gmail.com>
2026-04-28 15:54:27 +0530

    completed todo

--- script/script.js
+++ script/script.js
@@ -8,7 +8,69 @@ const themeToggleBtn = document.querySelector(".toggle-theme");
 
 themeToggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("light");
+    const theme = document.body.classList.contains("light") ? "light" : "dark";
+    localStorage.setItem("theme", theme);
 });
+
+function loadTheme() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "light") {
+        document.body.classList.add("light");
+    }
+}
+
+function saveTasks() {
+    const tasks = [];
+
+    const allTasks = document.querySelectorAll(".task");
+
+    allTasks.forEach(task => {
+        const text = task.querySelector("p").innerText;
+        const status = task.classList.contains("status");
+
+        tasks.push({ text, status });
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function createTask(textValue, isStatus = false) {
+    const task = document.createElement("div");
+    task.classList.add("task");
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+
+    const text = document.createElement("p");
+    text.innerText = textValue;
+
+    if (isStatus ) {
+        task.classList.add("status");
+        circle.classList.add("completed");
+    }
+
+    task.appendChild(circle);
+    task.appendChild(text);
+    list.appendChild(task);
+
+    circle.addEventListener("click", () => {
+        circle.classList.toggle("completed");
+        task.classList.toggle("status");
+        updateItemsLeft();
+        filterTasks(getActiveFilter());
+        saveTasks();
+    });
+}
+
+function loadTasks() {
+    const data = JSON.parse(localStorage.getItem("tasks")) || [];
+    data.forEach(item => {
+        createTask(item.text, item.status);
+    });
+    updateItemsLeft();
+    filterTasks(getActiveFilter());
+}
+
 function getActiveFilter() {
     return document.querySelector(".filter button.active")?.getAttribute("data-filter") || "all";
 }
@@ -18,22 +80,29 @@ function updateItemsLeft() {
     let count = 0;
 
     tasks.forEach(task => {
-        if (!task.classList.contains("done")) count++;
+        if (!task.classList.contains("status")) count++;
     });
 
     itemsLeftText.innerText = `${count} items left`;
+
+    const footer = document.querySelector(".todo-footer");
+
+    if (tasks.length === 0) {
+        footer.style.display = "none";
+    } else {
+        footer.style.display = "";
+    }
 }
 
 function filterTasks(type) {
     const tasks = document.querySelectorAll(".task");
-
     tasks.forEach(task => {
         if (type === "all") {
             task.style.display = "flex";
         } else if (type === "active") {
-            task.style.display = task.classList.contains("done") ? "none" : "flex";
-        } else if (type === "completed") {
-            task.style.display = task.classList.contains("done") ? "flex" : "none";
+            task.style.display = task.classList.contains("status") ? "none" : "flex";
+        } else {
+            task.style.display = task.classList.contains("status") ? "flex" : "none";
         }
     });
 }
@@ -41,32 +110,11 @@ function filterTasks(type) {
 function addTask() {
     const taskText = input.value.trim();
     if (!taskText) return;
-
-    const task = document.createElement("div");
-    task.classList.add("task");
-
-    const circle = document.createElement("span");
-    circle.classList.add("circle");
-
-    const text = document.createElement("p");
-    text.innerText = taskText;
-
-    task.appendChild(circle);
-    task.appendChild(text);
-    list.appendChild(task);
-
+    createTask(taskText);
     input.value = "";
-
     updateItemsLeft();
     filterTasks(getActiveFilter());
-
-    circle.addEventListener("click", () => {
-        circle.classList.toggle("completed");
-        task.classList.toggle("done");
-
-        updateItemsLeft();
-        filterTasks(getActiveFilter());
-    });
+    saveTasks();
 }
 
 addBtn.addEventListener("click", addTask);
@@ -84,9 +132,12 @@ filters.forEach(btn => {
 });
 
 clearCompletedBtn.addEventListener("click", () => {
-    document.querySelectorAll(".task.done").forEach(task => task.remove());
+    document.querySelectorAll(".task.status").forEach(task => task.remove());
+
     updateItemsLeft();
     filterTasks(getActiveFilter());
+    saveTasks();
 });
 
-updateItemsLeft();
+loadTheme();
+loadTasks();
